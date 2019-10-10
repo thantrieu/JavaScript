e37ea8ee29fabdd5d6f86167103c5aa3ea293e85
Bino <trieuthan92pro@gmail.com>
2019-10-10 07:22:58 +0700

    js best practices

---
+++ reversed.js
@@ -0,0 +1,15 @@
+// không sử dung được các biến, các đối tượng không khai báo
+// không sử dụng được các từ khóa sau làm tên biến
+implements
+interface
+let
+package
+private
+protected
+public
+static
+yield
+// không sử dụng được 'arguments' làm tên biến
+// không được lặp tên tham số
+// không sử dụng được 'delete'
+// 
